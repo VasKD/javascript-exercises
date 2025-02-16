5050f9c10e8e30b940c0d5033498669b516dbb3e
VasKD <kiara.denisse96@gmail.com>
2025-02-16 11:22:15 -0500

    Exercise 2 Solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text, repeatNum) {
+    string = "";
+    if (repeatNum == 0) {
+        return string;
+    } else if (repeatNum < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < repeatNum; i++) {
+        string += text;
+    }
+    return string;
 };
 
 // Do not edit below this line
