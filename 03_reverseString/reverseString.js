7e7743d036a8795cee787ebd44ad4c8fc33302d3
VasKD <kiara.denisse96@gmail.com>
2025-02-16 11:46:06 -0500

    Alternate Solution Added

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,14 +1,17 @@
 const reverseString = function(text) {
-    string = "";
-    // split string into characters
-    textArray = text.split("");
+    // string = "";
+    // // split string into characters
+    // textArray = text.split("");
 
-    // iterate through array backwards and add to string to obtain reverse string
-    for (let i = textArray.length - 1; i > -1; i--){
-        string += textArray[i];
-    }
+    // // iterate through array backwards and add to string to obtain reverse string
+    // for (let i = textArray.length - 1; i > -1; i--){
+    //     string += textArray[i];
+    // }
     
-    return string;
+    // return string;
+
+    // Alternate Solution (complexity is O(n) instead of O(n^2))
+    return text.split("").reverse().join("");
 };
 
 // Do not edit below this line
