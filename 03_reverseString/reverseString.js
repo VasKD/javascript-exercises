8a008d06cbcc40de727242e0a96567596dac9ef3
VasKD <kiara.denisse96@gmail.com>
2025-02-16 11:39:38 -0500

    Exercise 3 Solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    string = "";
+    // split string into characters
+    textArray = text.split("");
 
+    // iterate through array backwards and add to string to obtain reverse string
+    for (let i = textArray.length - 1; i > -1; i--){
+        string += textArray[i];
+    }
+    
+    return string;
 };
 
 // Do not edit below this line
