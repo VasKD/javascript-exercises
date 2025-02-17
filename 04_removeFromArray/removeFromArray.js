ff746295929a60eb5feaef8dc5b7735c3f0e0724
VasKD <kiara.denisse96@gmail.com>
2025-02-16 19:45:59 -0500

    Exercise 4 Solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, number, ...rest) {
+    let valuesToRemove = [number, ...rest];
+    // filter through the array in one pass by keeping all the elements
+    // that are NOT in valuesToRemove
+    return array.filter(item => !valuesToRemove.includes(item));
 };
 
 // Do not edit below this line
