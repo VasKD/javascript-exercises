217f5999db7459fb5995a8efd2cfa926d7e85038
VasKD <kiara.denisse96@gmail.com>
2025-02-16 20:15:52 -0500

    Exercise 5 Solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let numOfInts = 0;
 
+    // return error if inputs are non-integers
+    if (num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR"
+    }
+    
+    // check if larger number is first and adjust if not
+    if (num1 < num2){
+        numOfInts = (num2 - num1) + 1;
+    } else {
+        numOfInts = (num1 - num2) + 1; 
+    }
+    
+    let average = (num1 + num2) / 2;
+    sum = average * numOfInts;
+
+    return sum;
 };
 
 // Do not edit below this line
