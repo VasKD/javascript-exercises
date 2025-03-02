ddb3c81edba61a099e6760cce68061d7c68c14c8
VasKD <kiara.denisse96@gmail.com>
2025-03-01 22:32:56 -0500

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    // make sure input is a number
+    num = parseInt(num);
 
+    // address edge cases
+    if (num < 0) {
+        return "OOPS"
+    } else   if (num == 0){
+        return 0;
+    } else if (num <= 2) {
+        return 1;
+    }
+
+    let sum = 0;
+    let sequence = [1, 1];
+
+    // work through the sequence until the desired number is acquired
+    for (let i = 1; sequence.length < num; i++) {
+        sum = sequence[i] + sequence[i - 1];
+        sequence.push(sum);
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
