5eee3c9f3d5456abdc26b570eb1bec6d6c0312f3
VasKD <kiara.denisse96@gmail.com>
2025-03-01 21:54:20 -0500

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // convert the string to lower case, 
+    // split letters into an array and filter out invalid characters
+    let arr = str.toLowerCase().split('').filter(char => /[a-z0-9]/.test(char));
 
+    // compare original with reversed string anf
+    return arr.join('') === arr.reverse().join('');
 };
 
 // Do not edit below this line
