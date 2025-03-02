3d55b2f63b63876de25580795843bd2afe684a3d
VasKD <kiara.denisse96@gmail.com>
2025-03-02 12:27:30 -0500

    solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    // find the people without a death year and set it to the current year
+    people = people.filter(person => person.yearOfDeath === undefined ? person.yearOfDeath = new Date().getFullYear() : person);
+    // compare each of the ages and return the oldest
+    const oldest = people.reduce((largest, current) => (current.yearOfDeath - current.yearOfBirth > largest.yearOfDeath - largest.yearOfBirth ? current : largest), people[0]);
+    return oldest;
 };
 
 // Do not edit below this line
